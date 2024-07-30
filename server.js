d951f9d21643257300aae8f0e122bfeebf561574
Sujan Dhakal <101464442+sujandhakal0@users.noreply.github.com>
2024-07-30 12:43:06 +0545

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import app from "./app.js";
 import cloudinary from "cloudinary";
-
+const port = process.env.PORT || 4000
 cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
+    
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
   console.log(`http://localhost:${process.env.PORT}`);
 });
