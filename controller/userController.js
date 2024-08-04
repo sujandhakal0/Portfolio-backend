cf68e122192dc319450283b00b5bd8899a0d8f36
Sujan Dhakal <101464442+sujandhakal0@users.noreply.github.com>
2024-08-04 10:44:17 +0545

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -189,7 +189,7 @@ export const updatePassword = catchAsyncErrors(async (req, res, next) => {
 });
 
 export const getUserForPortfolio = catchAsyncErrors(async (req, res, next) => {
-  const id = "669e87244545efb7f4ec2ed4";
+  const id = "66a48d39735d0bd8e9896778";
   const user = await User.findById(id);
 
   res.status(200).json({
