c251bda7151d617e7392b26ca052cf06dafeded2
Sujan Dhakal <101464442+sujandhakal0@users.noreply.github.com>
2024-08-04 10:24:42 +0545

    Update userRouter.js

--- router/userRouter.js
+++ router/userRouter.js
@@ -17,7 +17,7 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", isAuthenticated, logout);
-router.get("/me", isAuthenticated, getUser);
+router.get("/me", getUser);
 router.put("/update/me", isAuthenticated, updateProfile);
 router.put("/update/password", isAuthenticated, updatePassword);
 router.get("/me/portfolio", getUserForPortfolio);
